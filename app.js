2ce8095fc8b63daccece9ed8e90a7b27a7683019
subhan215 <63653805+subhan215@users.noreply.github.com>
2021-12-03 11:14:21 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+// Answer 1 //
+/* var mainContent = document.getElementById("main-content");
+var childsMainContent = mainContent.childNodes;
+console.log(childsMainContent); 
+var render = document.getElementsByClassName("render");
+for(var i = 0; i < render.length; i++){
+    document.write(render[i].innerHTML + "<br>");
+}
+var firstName = document.getElementById("first-name");
+console.log(firstName.value = "subhan");
+var LastName = document.getElementById("last-name");
+console.log(LastName.value = "Rangila");
+var Email = document.getElementById("email");
+console.log(Email.value = "asifsubhan904@gmail.com"); */
+
+// Answer 2 //
+/* var formContent = document.getElementById("form-content").nodeType;
+console.log(formContent);
+var LastNameType = document.getElementById("lastName").nodeType;
+console.log(LastNameType);
+var childsLastName = document.getElementById("lastName").childNodes;
+console.log(childsLastName);
+var lastNameElement = document.getElementById("lastName");
+var element =  document.createElement("p");
+lastNameElement.appendChild(element);
+console.log(lastNameElement.childNodes);
+var firstChild = document.getElementById("main-content").firstChild;
+var lastChild = document.getElementById("main-content").lastChild;
+console.log(firstChild);
+console.log(lastChild);
+console.log(lastNameElement.nextSibling);
+console.log(lastNameElement.previousSibling);
+var email = document.getElementById("email");
+console.log(email.nodeType);
+console.log(email.parentNode); */
